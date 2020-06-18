84136b316132aa938af1f0b4674c9f875e031b01
VladBohdan <darekz04111995@gmail.com>
2020-06-18 16:08:28 +0300

    Derivation of Post

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PostsService} from '../shared/posts.service';
+import {Observable} from 'rxjs';
+import {Post} from '../shared/interfaces';
 
 @Component({
-  selector: 'app-home-page',
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+    selector: 'app-home-page',
+    templateUrl: './home-page.component.html',
+    styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+    posts$: Observable<Post[]>;
 
-  ngOnInit() {
-  }
+    constructor(private postService: PostsService) {
+    }
+
+    ngOnInit() {
+        this.posts$ = this.postService.getAll();
+    }
 
 }
