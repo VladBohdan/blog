4c2b758a14fc578e565f6e0cd89e7424f12d937f
VladBohdan <darekz04111995@gmail.com>
2020-07-03 16:10:31 +0300

    test

--- src/app/admin/shared/get-check-box/get-check-box.component.ts
+++ src/app/admin/shared/get-check-box/get-check-box.component.ts
@@ -1,49 +1,27 @@
 import { Component } from '@angular/core';
-import {ThemePalette} from '@angular/material/core';
 
-
-export interface Task {
-  name: string;
-  completed: boolean;
-  color: ThemePalette;
-  subtasks?: Task[];
-}
 @Component({
   selector: 'app-get-check-box',
   templateUrl: './get-check-box.component.html',
   styleUrls: ['./get-check-box.component.scss']
 })
 export class GetCheckBoxComponent {
+  lastAction: string;
 
-  task: Task = {
-    name: 'Усі категорії',
-    completed: false,
-    color: 'primary',
-    subtasks: [
-      {name: 'Title', completed: false, color: 'primary'},
-      {name: 'Tag', completed: false, color: 'accent'},
-    ]
-  };
+  field = [
+    { label: 'title', checked: false },
+    { label: 'tag', checked: false }
+  ];
 
-  allComplete = false;
+  onChange(event, index, item) {
 
-  updateAllComplete() {
-    this.allComplete = this.task.subtasks != null && this.task.subtasks.every(t => t.completed);
+    item.checked = !item.checked;
+
+    this.lastAction = 'index: ' + index + ', label: ' + item.label + ', checked: ' + item.checked;
+
+    console.log(index, event, item);
+
   }
 
-  someComplete(): boolean {
-    if (this.task.subtasks == null) {
-      return false;
-    }
-    return this.task.subtasks.filter(t => t.completed).length > 0 && !this.allComplete;
-  }
-
-  setAll(completed: boolean) {
-    this.allComplete = completed;
-    if (this.task.subtasks == null) {
-      return;
-    }
-    this.task.subtasks.forEach(t => t.completed = completed);
-  }
 
 }
